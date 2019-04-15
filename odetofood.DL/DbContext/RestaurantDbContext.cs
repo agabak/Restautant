47fd10a039dbddea2ce13c8ccb72da25f8a95a46
Agaba Kabunga <agaba_k@hotmail.com>
2019-04-15 10:59:34 -0500

    Add dl and ui

---
+++ odetofood.DL/DbContext/RestaurantDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using odetofood.DL.Models;
+
+namespace odetofood.DL
+{
+    public class RestaurantDbContext: DbContext
+    {
+        public RestaurantDbContext()
+            : base("DefaultConnection")
+        {}
+
+          DbSet<Restaurant> Restaurants { get; set; }
+          DbSet<RestaurantReview> RestaurantsReviews { get; set; }
+    }
+}
